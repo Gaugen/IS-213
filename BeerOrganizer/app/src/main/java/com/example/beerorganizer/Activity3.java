8d823117ececf1ae9d8044e292aa3c8289c4a4f7
simen stromland <simenstromland@gmail.com>
2015-12-03 10:42:30 +0100

    Logg
    Laget layout og loggfil til logg

--- BeerOrganizer/app/src/main/java/com/example/beerorganizer/Activity3.java
+++
@@ -1,7 +0,0 @@
-package com.example.beerorganizer;
-
-/**
- * Created by TorArne on 12/1/2015.
- */
-public class Activity3 {
-}
---
+++ BeerOrganizer/app/src/main/java/com/example/beerorganizer/Logg.java
@@ -0,0 +1,18 @@
+package com.example.beerorganizer;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+/**
+ * Created by TorArne on 12/1/2015.
+ */
+public class Logg {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logg;
+        SharedPreferences log = getSharedPreferences("LogPreference", Activity.MODE_PRIVATE);
+
+
+}
